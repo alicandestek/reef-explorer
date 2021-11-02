16fd2ed997fc17453b36f37f0b034dd6f40655c1
mariopino <mariopino@protonmail.com>
2021-11-02 10:22:30 +0100

    debug

--- backend/crawlers/activeAccounts.js
+++ backend/crawlers/activeAccounts.js
@@ -34,88 +34,93 @@ const chunker = (a, n) => Array.from(
   (_, i) => a.slice(i * n, i * n + n),
 );
 
-const processChunk = async (api, client, accountId) => {
-  const timestamp = Math.floor(parseInt(Date.now().toString(), 10) / 1000);
-  const [block, identity, balances, chainEvmAddress] = await Promise.all([
-    api.rpc.chain.getBlock().then((result) => result.block.header.number.toNumber()),
-    api.derive.accounts.identity(accountId),
-    api.derive.balances.all(accountId),
-    api.query.evmAccounts.evmAddresses(accountId),
-  ]);
-  const availableBalance = balances.availableBalance.toString();
-  const freeBalance = balances.freeBalance.toString();
-  const lockedBalance = balances.lockedBalance.toString();
-  const identityDisplay = identity.display ? identity.display.toString() : '';
-  const identityDisplayParent = identity.displayParent ? identity.displayParent.toString() : '';
-  const JSONIdentity = identity.display ? JSON.stringify(identity) : '';
-  const JSONbalances = JSON.stringify(balances);
-  const nonce = balances.accountNonce.toString();
-  const evmAddress = chainEvmAddress.toString() !== '' ? toChecksumAddress(chainEvmAddress.toString()) : null;
-  const evmNonce = evmAddress ? await api.query.evm.accounts(evmAddress).then((res) => res.toJSON()?.nonce || 0) : null;
+const processAccount = async (api, client, accountId) => {
+  logger.info(loggerOptions, `Processing account: ${accountId}`);
+  try {
+    const timestamp = Math.floor(parseInt(Date.now().toString(), 10) / 1000);
+    const [block, identity, balances, chainEvmAddress] = await Promise.all([
+      api.rpc.chain.getBlock().then((result) => result.block.header.number.toNumber()),
+      api.derive.accounts.identity(accountId),
+      api.derive.balances.all(accountId),
+      api.query.evmAccounts.evmAddresses(accountId),
+    ]);
+    const availableBalance = balances.availableBalance.toString();
+    const freeBalance = balances.freeBalance.toString();
+    const lockedBalance = balances.lockedBalance.toString();
+    const identityDisplay = identity.display ? identity.display.toString() : '';
+    const identityDisplayParent = identity.displayParent ? identity.displayParent.toString() : '';
+    const JSONIdentity = identity.display ? JSON.stringify(identity) : '';
+    const JSONbalances = JSON.stringify(balances);
+    const nonce = balances.accountNonce.toString();
+    const evmAddress = chainEvmAddress.toString() !== '' ? toChecksumAddress(chainEvmAddress.toString()) : null;
+    const evmNonce = evmAddress ? await api.query.evm.accounts(evmAddress).then((res) => res.toJSON()?.nonce || 0) : null;
 
-  const data = [
-    accountId,
-    JSONIdentity,
-    identityDisplay,
-    identityDisplayParent,
-    JSONbalances,
-    availableBalance,
-    freeBalance,
-    lockedBalance,
-    nonce,
-    timestamp,
-    block,
-    evmAddress,
-    evmNonce
-  ];
-  const query = `
-    INSERT INTO account (
-      account_id,
-      identity,
-      identity_display,
-      identity_display_parent,
-      balances,
-      available_balance,
-      free_balance,
-      locked_balance,
+    const data = [
+      accountId,
+      JSONIdentity,
+      identityDisplay,
+      identityDisplayParent,
+      JSONbalances,
+      availableBalance,
+      freeBalance,
+      lockedBalance,
       nonce,
       timestamp,
-      block_height,
-      evm_address,
-      evm_nonce
-    ) VALUES (
-      $1,
-      $2,
-      $3,
-      $4,
-      $5,
-      $6,
-      $7,
-      $8,
-      $9,
-      $10,
-      $11,
-      $12,
-      $13
-    )
-    ON CONFLICT (account_id)
-    DO UPDATE SET
-      identity = EXCLUDED.identity,
-      identity_display = EXCLUDED.identity_display,
-      identity_display_parent = EXCLUDED.identity_display_parent,
-      balances = EXCLUDED.balances,
-      available_balance = EXCLUDED.available_balance,
-      free_balance = EXCLUDED.free_balance,
-      locked_balance = EXCLUDED.locked_balance,
-      nonce = EXCLUDED.nonce,
-      timestamp = EXCLUDED.timestamp,
-      block_height = EXCLUDED.block_height,
-      evm_address = EXCLUDED.evm_address,
-      evm_nonce = EXCLUDED.evm_nonce
-    WHERE EXCLUDED.block_height > account.block_height
-  ;`;
-  // eslint-disable-next-line no-await-in-loop
-  await dbParamQuery(client, query, data, loggerOptions);
+      block,
+      evmAddress,
+      evmNonce
+    ];
+    const query = `
+      INSERT INTO account (
+        account_id,
+        identity,
+        identity_display,
+        identity_display_parent,
+        balances,
+        available_balance,
+        free_balance,
+        locked_balance,
+        nonce,
+        timestamp,
+        block_height,
+        evm_address,
+        evm_nonce
+      ) VALUES (
+        $1,
+        $2,
+        $3,
+        $4,
+        $5,
+        $6,
+        $7,
+        $8,
+        $9,
+        $10,
+        $11,
+        $12,
+        $13
+      )
+      ON CONFLICT (account_id)
+      DO UPDATE SET
+        identity = EXCLUDED.identity,
+        identity_display = EXCLUDED.identity_display,
+        identity_display_parent = EXCLUDED.identity_display_parent,
+        balances = EXCLUDED.balances,
+        available_balance = EXCLUDED.available_balance,
+        free_balance = EXCLUDED.free_balance,
+        locked_balance = EXCLUDED.locked_balance,
+        nonce = EXCLUDED.nonce,
+        timestamp = EXCLUDED.timestamp,
+        block_height = EXCLUDED.block_height,
+        evm_address = EXCLUDED.evm_address,
+        evm_nonce = EXCLUDED.evm_nonce
+      WHERE EXCLUDED.block_height > account.block_height
+    ;`;
+    // eslint-disable-next-line no-await-in-loop
+    await dbParamQuery(client, query, data, loggerOptions);
+  } catch (error) {
+    logger.info(loggerOptions, `Error processing accountId ${accountId}: ${error?.stack || error}`);
+  }
 };
 
 const crawler = async (delayedStart) => {
@@ -149,7 +154,7 @@ const crawler = async (delayedStart) => {
     // eslint-disable-next-line no-await-in-loop
     await Promise.all(
       chunk.map(
-        (accountId) => processChunk(api, client, accountId),
+        (accountId) => processAccount(api, client, accountId),
       ),
     );
     const chunkEndTime = new Date().getTime();
